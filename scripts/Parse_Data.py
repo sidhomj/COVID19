8ae819dea3170d8aaef0561f447d340e826f31dd
JW <sidhomj@gmail.com>
2020-06-16 23:11:41 -0400

    update

--- scripts/Parse_Data.py
+++ scripts/Parse_Data.py
@@ -1,4 +1,3 @@
-from DeepTCR.DeepTCR import DeepTCR_SS
 import pandas as pd
 import numpy as np
 import os
@@ -23,8 +22,6 @@ file = '../Data/peptide-detail.csv'
 data = pd.read_csv(file)
 cdr3 = data['TCR BioIdentity'].str.split('+',expand=True)
 data['beta_sequences'] = np.array(cdr3[0])
-data['v_beta'] = np.array(cdr3[1])
-data['j_beta'] = np.array(cdr3[2])
 data = Process_Seq(data,'beta_sequences')
 
 peptides = data['Amino Acids'].str.split(',',expand=True)
@@ -39,8 +36,6 @@ for ii,pep in enumerate(peptides.iterrows(),0):
     exp_list.append([data['Experiment'].iloc[ii]]*len(pep))
 
 beta_sequences_list = np.hstack(beta_sequences_list)
-v_beta_list = np.hstack(v_beta_list)
-j_beta_list = np.hstack(j_beta_list)
 peptide_list = np.hstack(peptide_list)
 exp_list = np.hstack(exp_list)
 
@@ -49,14 +44,14 @@ data['beta_sequences'] = beta_sequences_list
 data['peptide'] = peptide_list
 data['Experiment'] = exp_list
 
-meta = pd.read_csv('Data/subject-metadata.csv')
+meta = pd.read_csv('../Data/subject-metadata.csv')
 meta['Subject'] = 's_'+meta['Subject'].astype(str)
 sample_dict = dict(zip(meta['Experiment'],meta['Subject']))
 data['Subject'] = data['Experiment'].map(sample_dict)
 covid_dict = dict(zip(meta['Subject'],meta['Cohort']))
 data['Cohort'] = data['Subject'].map(covid_dict)
 
-df_orf = pd.read_csv('Data/minigene-hits.csv')
+df_orf = pd.read_csv('../Data/minigene-hits.csv')
 orfs = np.array(df_orf['Amino Acid'])
 
 orf_list = []
@@ -71,4 +66,4 @@ data['orf'] = orf_list
 orf_dict = dict(zip(df_orf['Amino Acid'],df_orf['ORF']))
 orf_dict['ORF1ab'] = 'ORF1ab'
 data['orf_name'] = data['orf'].map(orf_dict)
-data.to_csv('Data/data_parsed.csv',index=False)
+data.to_csv('../Data/data_parsed.csv',index=False)
