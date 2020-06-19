fd4a2fb89ea92cfeba90d260228f2644f437c8b3
JW <sidhomj@gmail.com>
2020-06-19 14:30:48 -0400

    update

--- scripts/Parse_Data.py
+++ scripts/Parse_Data.py
@@ -1,17 +1,21 @@
 """ This script takes the data from Adaptive ImmuneCode and assembles it into a
-dataframe that maps TCR's to their respective subject, cohort, ORF. """
+dataframe that maps TCRs to their respective subject, cohort, ORF. """
 
 import pandas as pd
 import numpy as np
 import os
 from utils import *
 
+#Import peptide data from Adaptive
 file = '../Data/peptide-detail.csv'
 data = pd.read_csv(file)
 cdr3 = data['TCR BioIdentity'].str.split('+',expand=True)
 data['beta_sequences'] = np.array(cdr3[0])
+
+#Process TCRs
 data = Process_Seq(data,'beta_sequences')
 
+#Map TCRs to peptides
 peptides = data['Amino Acids'].str.split(',',expand=True)
 peptide_list = []
 beta_sequences_list = []
@@ -26,12 +30,12 @@ for ii,pep in enumerate(peptides.iterrows(),0):
 beta_sequences_list = np.hstack(beta_sequences_list)
 peptide_list = np.hstack(peptide_list)
 exp_list = np.hstack(exp_list)
-
 data = pd.DataFrame()
 data['beta_sequences'] = beta_sequences_list
 data['peptide'] = peptide_list
 data['Experiment'] = exp_list
 
+#Add meta data
 meta = pd.read_csv('../Data/subject-metadata.csv')
 meta['Subject'] = 's_'+meta['Subject'].astype(str)
 sample_dict = dict(zip(meta['Experiment'],meta['Subject']))
@@ -39,6 +43,7 @@ data['Subject'] = data['Experiment'].map(sample_dict)
 covid_dict = dict(zip(meta['Subject'],meta['Cohort']))
 data['Cohort'] = data['Subject'].map(covid_dict)
 
+#Add ORF data
 df_orf = pd.read_csv('../Data/minigene-hits.csv')
 orfs = np.array(df_orf['Amino Acid'])
 
@@ -50,9 +55,10 @@ for pep in data['peptide']:
         orf_list.append('ORF1ab')
 
 data['orf'] = orf_list
-
 orf_dict = dict(zip(df_orf['Amino Acid'],df_orf['ORF']))
 orf_dict['ORF1ab'] = 'ORF1ab'
 data['orf_name'] = data['orf'].map(orf_dict)
+
+#Drop duplicates of beta/peptide pairs in patients
 data.drop_duplicates(subset=['beta_sequences','peptide','Subject'],inplace=True)
 data.to_csv('../Data/data_parsed.csv',index=False)
